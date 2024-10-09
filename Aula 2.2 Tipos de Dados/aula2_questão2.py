97b1b3e7ecb6ca0e3c622d7caca06498ed282290
WillyansGuia <153099191+WillyansGuia@users.noreply.github.com>
2024-10-09 11:49:12 -0300

    Add files via upload

---
+++ Aula 2.2 Tipos de Dados/aula2_questão2.py
@@ -0,0 +1,19 @@
+#Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+solução = "o resultado é"
+v1 = 10
+v2 = 3.5 
+soma = v1 + v2
+print(solução)
+print(soma)
+
+print(type(solução))
+print(type(v1))
+print(type(v2))
+print(type(soma))
+
+
+
